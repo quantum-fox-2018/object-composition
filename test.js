61f56115f437d8c627e4fa71d893503aecff57a2
marcosumali <marcosumali.github.io>
2018-03-13 21:18:10 +0700

    add release - ....]

---
+++ test.js
@@ -0,0 +1,4 @@
+var text = '2 tsp,butter,1 cup,cinnamon,2 cups,peanut butter,2 cups (gluten),sugar,1 cup,flour,'
+console.log(text.length);
+// console.log(text[82])
+console.log(text.slice(0,text.length-1));
